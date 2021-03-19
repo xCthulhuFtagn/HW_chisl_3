449c0e981d9e63c53a67df0fea1cef2d4b50f8b2
Максим Колодяжный <mkolodyazhny@gmail.com>
2021-03-19 18:19:33 +0300

    Наконец!

--- HW_chisl_3/HW_chisl_3.cpp
+++ HW_chisl_3/HW_chisl_3.cpp
@@ -1,20 +1,47 @@
-﻿// HW_chisl_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <vector>
 #include <iostream>
+#include <iomanip>
+using namespace std;
+
+double Lagrange(double param, vector<double>* data) {
+	double tmp, LagPol = 0;
+	unsigned n = data[0].size();
+	for (unsigned i = 0; i < n; ++i) {
+		tmp = data[1][i];
+		for (unsigned j = 0; j < n; ++j) {
+			if (i == j) continue;
+			tmp *= (param - data[0][j]) / (data[0][i] - data[0][j]);
+		}
+		LagPol += tmp;
+	}
+	return LagPol;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	unsigned n, size;
+	double LagPol = 0, x, y, param, tmp;
+	vector<double> data[2];
+	try {
+		cout << "Enter number of input: ";
+		cin >> n;
+		if (n == 0 || !cin) throw invalid_argument("wrong number");
+		cout << "Enter data:" << endl;
+		for (unsigned i = 0; i < n; ++i) {
+			cin >> x >> y;
+			if (!cin) throw invalid_argument("wrong data");
+			data[0].push_back(x);
+			data[1].push_back(y);
+		}
+	}
+	catch (invalid_argument bad) {
+		cout << bad.what();
+		return -1;
+	}
+	size = data[0].size();
+	cout << "x\t|\ty\t|\tans" << endl;
+	for (unsigned i = 0; i < size; ++i) {
+		cout << data[0][i] << "\t|\t" << data[1][i] << "\t|\t" << Lagrange(data[0][i], data) << endl;
+	}
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
