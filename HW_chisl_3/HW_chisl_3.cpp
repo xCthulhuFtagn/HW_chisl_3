ba4b0441e462ae4592cefe03f7a8b9710a6d9d5a
Максим Колодяжный <mkolodyazhny@gmail.com>
2021-03-21 20:07:38 +0300

    новая версия Spline

--- HW_chisl_3/HW_chisl_3.cpp
+++ HW_chisl_3/HW_chisl_3.cpp
@@ -73,25 +73,52 @@ vector<double> Gaus(vector <valarray<double>> a) {
 	return ans;
 }
 
+	/*	vector <double> h(size), P(size), Q(size), m(size);
+        for (unsigned i = 1; i < size; ++i)
+            h[i] = X[i] - X[i-1];
+        P[1] = 0; Q[1] = 0;
+        for (unsigned i = 1; i + 1 < size; ++i){
+            P[i + 1] = - h[i+1] / (2 * (h[i] + h[i+1]) + P[i] * h[i]);
+            tmp = (Y[i+1] - Y[i]) / h[i+1] + (Y[i] - Y[i-1]) / h[i];
+            Q[i + 1] = (6 * tmp - h[i] * Q[i]) / (2 * (h[i] + h[i+1]) + P[i] * h[i]);
+        }
+        m[size - 1] = 0; m[0] = 0;
+        for (unsigned i = size - 1; i > 0; --i)
+            m[i - 1] = P[i] * m[i] + Q[i];
+
+        unsigned l = 0, r = size - 1;
+        while (r - l > 1){
+            unsigned m = (l + r) / 2;
+            if (X[m] > x)
+                r = m;
+            else
+                l = m;
+        }
+        ans = m[l+1] * pow(x - X[l], 3) / (6*h[l+1]) + m[l] * pow(X[l+1] - x, 3) / (6*h[l+1]) + (Y[l+1] - m[l+1] * h[l+1]*h[l+1] / 6) * (x - X[l]) / h[l+1] + (Y[l] - m[l] * h[l+1]*h[l+1] / 6) * (X[l+1] - x) / h[l+1];
+        cout << "Value in selected point calculated by splines: " << ans << endl;*/
+
 double Spline(double param, vector<double> *input) {
 	if (param <= input[0][0]) throw invalid_argument("can't find Splines for such argument!");
 	vector<double> h, m;
 	double ans;
 	unsigned size = input[0].size(), i;
-	vector<valarray<double>> output(size-2);
+	vector<valarray<double>> output(size);
 	for (unsigned i = 0; i < size-1; ++i) {
 		h.push_back(input[0][i + 1] - input[0][i]);
 	}
 	for (i = 0; i < size-2; ++i) {
-		output[i].resize(size - 1, 0);
-		output[i][i] = (h[i] + h[i + 1]) / 3;
-		output[i][i + 1] = h[i + 1] / 6;
-		output[i][size-3] = (input[1][i + 2] - input[1][i + 1]) / h[i + 1] - (input[1][i + 1] - input[1][i]) / h[i];
+		output[i].resize(size + 1, 0);
+		output[i][i + 1] = (h[i] + h[i + 1]) / 3;
+		output[i][i + 2] = h[i + 1] / 6;
+		output[i][size] = (input[1][i + 2] - input[1][i + 1]) / h[i + 1] - (input[1][i + 1] - input[1][i]) / h[i];
 	}
+	output[i].resize(size + 1, 0);
+	output[i][0] = 1;
+	output[i+1].resize(size + 1, 0);
+	output[i+1][size-1] = 1;
 	m=Gaus(output);
-	int mm1, mm2;
 	for (i = 0; i < size && input[0][i] < param; ++i);
-	ans=((m[i] * pow((param - input[0][i]), 2) + m[i-1] * pow(input[0][i + 1] - param, 3)) / (h[i] * 6) + (input[1][i + 1] - m[i-1] * pow(h[i], 2) / 6) * ((param - input[0][i]) / h[i]) + (input[1][i] - m[i] * pow(h[i], 2) / 6) * (input[0][i] - param) / h[i]);
+	ans=((m[i] * pow((param - input[0][i-2]), 3) + m[i-1] * pow(input[0][i-1] - param, 3)) / (h[i-1] * 6) + (input[1][i-1] - m[i] * pow(h[i-1], 2) / 6) * ((param - input[0][i-2]) / h[i-1]) + (input[1][i-2] - m[i-1] * pow(h[i-1], 2) / 6) * (input[0][i-1] - param) / h[i-1]);
 	return ans;
 }
 
@@ -127,11 +154,9 @@ int main()
 	for (unsigned i = 0; i < size; ++i) {
 		cout << data[0][i] << "\t|\t" << data[1][i] << "\t|\t" << Newton(data[0][i], data) << endl;
 	}
-	cout << "Bias in 0.1875 is: " << Lagrange(0.1875, data)-Newton(0.1875, data)<<endl;
+	cout << "Bias in 0.1875 is: " << Lagrange(0.1875, data) - Newton(0.1875, data) << endl;
 	try {
-		for (unsigned i = 1; i < size-2; ++i) {
-			cout << Spline(data[0][i], data) << "\t";
-		}
+		cout << Spline(0.1875, data);
 	}
 	catch (invalid_argument& bad) {
 		cout << bad.what();
