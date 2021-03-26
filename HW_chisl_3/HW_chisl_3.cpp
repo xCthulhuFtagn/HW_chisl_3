6daa082dcdc3222de7ce5cc71668f7a9b1a6ad6c
Максим Колодяжный <mkolodyazhny@gmail.com>
2021-03-26 15:30:04 +0300

    added graphics, no testing

--- HW_chisl_3/HW_chisl_3.cpp
+++ HW_chisl_3/HW_chisl_3.cpp
@@ -2,10 +2,15 @@
 #include <iostream>
 #include <iomanip>
 #include <valarray>
-#include <math.h>
+#include <cmath>
 #include <exception>
+#include <algorithm>
+#include <windows.h>
+#include <conio.h>
+#undef min
+#undef max
 using namespace std;
-
+/*
 vector<double>& vector<double>::operator=(const vector<double>& right) {
 	this->assign(right.front(), right.back());
 }
@@ -17,7 +22,8 @@ void operator-=(vector<double>& left, const vector<double>& right) {
 			left[i] -= right[i];
 		}
 	}
-}
+}*/
+
 vector<double> operator*(const vector<double>& left, const double right) {
 	vector<double> ans;
 	for (auto el : left) {
@@ -26,7 +32,7 @@ vector<double> operator*(const vector<double>& left, const double right) {
 	return ans;
 }
 
-double Lagrange(double param, vector<double>* data) {
+double Lagrange(valarray<double>* data, double param) {
 	double tmp, LagPol = 0;
 	unsigned n = data[0].size();
 	for (unsigned i = 0; i < n; ++i) {
@@ -40,7 +46,7 @@ double Lagrange(double param, vector<double>* data) {
 	return LagPol;
 }
 
-double Newton(double param, vector<double>* data) {
+double Newton(valarray<double>* data, double param) {
 	unsigned size = data[0].size(), i;
 	vector<vector<double>> f(size - 1);
 	double ans, tmp;
@@ -52,7 +58,7 @@ double Newton(double param, vector<double>* data) {
 			f[i].push_back((f[i - 1][j + 1] - f[i - 1][j]) / (data[0][j + i + 1] - data[0][j]));
 		}
 	}
-	ans = data[1].front();
+	ans = data[1][0];
 	for (unsigned i = 0; i < size - 1; ++i) {
 		tmp = f[i].front();
 		for (unsigned j = 0; j <= i; ++j) {
@@ -63,10 +69,10 @@ double Newton(double param, vector<double>* data) {
 	return ans;
 }
 
-vector<double> Gaus(vector <vector<double>> a) {
+valarray<double> Gaus(vector <valarray<double>> a) {
 	unsigned n = a.size();
 	double coef;
-	vector<double> ans;
+	valarray<double> ans(n);
 	for (unsigned i = 0; i < n; ++i) {
 		unsigned j;
 		for (j = i; j < n && a[j][i] == 0; ++j);
@@ -86,29 +92,30 @@ vector<double> Gaus(vector <vector<double>> a) {
 		}
 	}
 	for (unsigned i = 0; i < n; ++i) {
-		ans.push_back(a[i][n] / a[i][i]);
+		ans[i]=(a[i][n] / a[i][i]);
 	}
 	return ans;
 }
 
-double Spline(double param, vector<double>* input) {
-	if (param <= input[0][0]) throw invalid_argument("can't find Splines for such argument!");
-	vector<double> h, m;
+double Spline(valarray<double>* input, double param) {
+	if (param <= input[0][0]) throw invalid_argument("Can't find Splines for such argument!");
+	vector<double> h;
 	double ans;
 	unsigned size = input[0].size(), i;
-	vector<vector<double>> output(size);
+	vector<valarray<double>> output(size);
+	valarray<double> m;
 	for (unsigned i = 0; i < size - 1; ++i) {
 		h.push_back(input[0][i + 1] - input[0][i]);
 	}
 	for (i = 0; i < size - 2; ++i) {
-		output[i].assign(size + 1, 0);
+		output[i].resize(size + 1, 0);
 		output[i][i + 1] = (h[i] + h[i + 1]) / 3;
 		output[i][i + 2] = h[i + 1] / 6;
 		output[i][size] = (input[1][i + 2] - input[1][i + 1]) / h[i + 1] - (input[1][i + 1] - input[1][i]) / h[i];
 	}
-	output[i].assign(size + 1, 0);
+	output[i].resize(size + 1, 0);
 	output[i][0] = 1;
-	output[i + 1].assign(size + 1, 0);
+	output[i + 1].resize(size + 1, 0);
 	output[i + 1][size - 1] = 1;
 	m = Gaus(output);
 	for (i = 0; i < size && input[0][i] < param; ++i);
@@ -117,9 +124,10 @@ double Spline(double param, vector<double>* input) {
 	return ans;
 }
 
-vector<double> SquareAproximation(vector<double>* data) {
+double SquareAproximation(valarray<double>* data, double x) {
 	unsigned size = data[0].size();
-	vector<vector<double>> suem(3);
+	vector<valarray<double>> suem(3);
+	valarray<double> abc;
 	vector<double> koef(7, 0);
 	for (unsigned i = 0; i < 7; ++i) {
 		if (i < 4) {
@@ -145,38 +153,31 @@ vector<double> SquareAproximation(vector<double>* data) {
 		}
 		suem[i][3] = koef[6 - i];
 	}
-	return Gaus(suem);
+	abc = Gaus(suem);
+	return abc[0] * pow(x, 2) + abc[1] * x + abc[2];
 }
 
-double SquareFunc(double a, double b, double c, double x) {
-	return a * pow(x, 2) + b * x + c;
-}
-
-double LinFunc(double k, double b, double x) {
-	return k*x + b;
-}
-
-vector<double> Linear_approximation(vector<double>* data)
+double LinearApproximation(valarray<double>* data, double x)
 {
 	unsigned size = data[0].size();
-	vector<double> koef(4, 0), ans;
-	//double x = 0, x2 = 0, y = 0, xy = 0;
+	vector<double> koef(4, 0);
+	valarray<double> ans;
 	for (int i = 0; i < 4; i++) {
 		koef[0] += data[0][i];
 		koef[1] += pow(data[0][i], 2);
 		koef[2] += data[1][i];
 		koef[3] += data[0][i] * data[1][i];
 	}
-	vector<vector<double>> syst(2);
+	vector<valarray<double>> syst(2);
 	for (auto& el : syst) el.resize(3);
-
 	syst[0][0] = koef[1];
 	syst[0][1] = koef[0];
 	syst[0][2] = koef[3];
 	syst[1][0] = koef[1];
 	syst[1][1] = size;
 	syst[1][2] = koef[2];
-	return Gaus(syst);
+	ans = Gaus(syst);
+	return ans[0] * x + ans[1];
 	//тут надо еще посмотреть
 	double Discrepancy = 0;
 	for (int i = 1; i <= size; i++) {
@@ -184,44 +185,107 @@ vector<double> Linear_approximation(vector<double>* data)
 	}
 }
 
+void Draw(valarray<double>* data, double(*f)(valarray<double>*, double)) {
+	Sleep(1000);
+	printf("\033[2J");
+	double x_max, x_min, diap_x;
+	double y_max, y_min, diap_y;
+	double y, x;
+	y_min = data[1].min(); x_min = data[0].min();
+	y_max = data[1].max(); x_max = data[0].max();
+	diap_x = (x_max - x_min) / 10;
+	diap_y = (y_max - y_min) / 10;                             //диапазон делений для оY 
+	y_max += diap_y; y_min -= diap_y;
+	HDC hDC = GetDC(GetConsoleWindow());                       //настройка консоли для рисования 
+	HPEN Pen = CreatePen(PS_SOLID, 1, RGB(255, 255, 255));     //ручка для разметки 
+	HPEN Pen1 = CreatePen(PS_SOLID, 2, RGB(0, 255, 0));        //ручка для графика 
+	HPEN Pen2 = CreatePen(PS_SOLID, 8, RGB(255, 0, 0));        //ручка для точек
+	if (true) {
+		HWND hwnd = GetConsoleWindow();
+		RECT rect;
+		//через прямоугольник rect описывается консолька 
+		GetWindowRect(hwnd, &rect);
+		int width = rect.right - rect.left;
+		int height = rect.bottom - rect.top;
+		SelectObject(hDC, Pen);
+		//оси координат 
+		MoveToEx(hDC, 0, height / 2, NULL);
+		LineTo(hDC, width, height / 2);
+		MoveToEx(hDC, width / 2, height, NULL);
+		LineTo(hDC, width / 2, 0);
+		//стрелка oY 
+		MoveToEx(hDC, width / 2, 0, NULL);
+		LineTo(hDC, width / 2 + 10, 10);
+		MoveToEx(hDC, width / 2 - 10, 10, NULL);
+		LineTo(hDC, width / 2, 0);
+		//стрелка оХ 
+		MoveToEx(hDC, width - 45, height / 2 - 10, NULL);
+		LineTo(hDC, width - 35, height / 2);
+		MoveToEx(hDC, width - 35, height / 2, NULL);
+		LineTo(hDC, width - 45, height / 2 + 10);
+		//деления oX
+		for (x = x_min; x <= x_max; x += diap_x) {
+			MoveToEx(hDC, width * (x - x_min) / (x_max - x_min), height / 2 - height / 250 - 1, NULL);
+			LineTo(hDC, width * (x - x_min) / (x_max - x_min), height / 2 + height / 250 + 1);
+		}
+		//деления oY
+		for (y = y_min; y < y_max + diap_y; y += diap_y) {
+			MoveToEx(hDC, width / 2 - width / 250 - 1, height * (y_max - y) / (y_max - y_min), NULL);
+			LineTo(hDC, width / 2 + width / 250 + 1, height * (y_max - y) / (y_max - y_min));
+		}
+		//вывод графика и точек, по которому он строится
+		SelectObject(hDC, Pen1);
+		for (x = x_min; x < x_max; x += 1e-3) {
+			MoveToEx(hDC, width * (x - x_min) / (x_max - x_min), height * (y_max - f(data, x)) / (y_max - y_min), NULL);
+			LineTo(hDC, width * (x + 1e-3 - x_min) / (x_max - x_min), height * (y_max - f(data, x + 1e-3)) / (y_max - y_min));
+		}
+		SelectObject(hDC, Pen2);
+		for (unsigned i = 0; i < data[0].size(); ++i) {
+			MoveToEx(hDC, width * (data[0][i] - x_min) / (x_max - x_min), height * (y_max - data[1][i]) / (y_max - y_min), NULL);
+			LineTo(hDC, width * (data[0][i] - x_min) / (x_max - x_min), height * (y_max - data[1][i]) / (y_max - y_min));
+		}
+	}
+	_getch();
+}
+
+
 int main()
 {
 	unsigned n, size;
 	double LagPol = 0, x, y, param, tmp;
-	vector<double> data[2], splines;
+	valarray<double> data[2], splines;
 	try {
 		cout << "Enter number of input: ";
 		cin >> n;
-		if (n == 0 || !cin) throw invalid_argument("wrong number");
+		if (n == 0 || !cin) throw invalid_argument("Wrong number");
+		data[0].resize(n);
+		data[1].resize(n);
 		cout << "Enter data:" << endl;
 		for (unsigned i = 0; i < n; ++i) {
 			cin >> x >> y;
-			if (!cin) throw invalid_argument("wrong data");
-			data[0].push_back(x);
-			data[1].push_back(y);
+			if (!cin) throw invalid_argument("Wrong data");
+			data[0][i]=(x);
+			data[1][i]=(y);
 		}
-	}
-	catch (invalid_argument bad) {
-		cout << bad.what();
-		return -1;
-	}
-	size = data[0].size();
-	cout << "Lagrange:" << endl;
-	cout << "x\t|\ty\t|\tans" << endl;
-	for (unsigned i = 0; i < size; ++i) {
-		cout << data[0][i] << "\t|\t" << data[1][i] << "\t|\t" << Lagrange(data[0][i], data) << endl;
-	}
-	cout << "Newton:" << endl;
-	cout << "x\t|\ty\t|\tans" << endl;
-	for (unsigned i = 0; i < size; ++i) {
-		cout << data[0][i] << "\t|\t" << data[1][i] << "\t|\t" << Newton(data[0][i], data) << endl;
-	}
-	cout << "Bias in 0.1875 is: " << Lagrange(0.1875, data) - Newton(0.1875, data) << endl;
-	try {
-		cout << Spline(0.1875, data);
+		cout << "Choose what you want to see:\nInterpolation by\n1)Newton\n2)Lagrange\n3)Splines\nApproximations :\n4)Quadratic\n5)Linear\n";
+		cin >> n;
+		if (!cin || n <= 0 || n > 4) throw invalid_argument("Wrong number entered while choosing what to see");
+		else {
+			switch (n) {
+			case 1:
+				Draw(data, Newton);
+			case 2:
+				Draw(data, Lagrange);
+			case 3:
+				Draw(data, SquareAproximation);
+			case 4:
+				Draw(data, LinearApproximation);
+			}
+		}
 	}
 	catch (invalid_argument& bad) {
 		cout << bad.what();
+		return -1;
 	}
 	return 0;
 }
